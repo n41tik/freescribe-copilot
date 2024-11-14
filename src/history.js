f261af7c12f72291cac7456779a96de81954baa3
Naitik Patel <n41tik@users.noreply.github.com>
2024-11-14 14:57:29 -0500

    adding history limit

--- src/history.js
+++ src/history.js
@@ -6,12 +6,12 @@ export async function saveNotesHistory(note) {
     note: note,
   };
 
-  chrome.storage.sync.set(
-    { notes_history: [new_note, ...notes_history] },
-    function () {
-      console.log("notes history saved");
-    }
-  );
+  // saving only last 20 notes
+  let new_notes_history = [new_note, ...notes_history].slice(0, 20);
+
+  chrome.storage.sync.set({ notes_history: new_notes_history }, function () {
+    console.log("notes history saved");
+  });
 }
 
 export async function getHistory() {
